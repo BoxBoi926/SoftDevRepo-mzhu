2b47c3d190df961768b3aa9dc4d6172ffeaabeef
markzhu926 <66125192+markzhu926@users.noreply.github.com>
2021-09-28 23:51:00 -0400

    Create random-occupation.py

---
+++ 06_py_csv/random-occupation.py
@@ -0,0 +1,24 @@
+# Mark Zhu, Justin Zou, Han Zhang (Team MJH)
+# SoftDev
+# K06 -- StI/O: Divine your Destiny!
+# 2021-09-28
+
+import random
+import csv
+
+
+randomNum = random.random()* 99.8
+dict = {}
+    
+with open("occupations.csv", mode = 'r') as csvfile:
+    csv = csv.DictReader(csvfile)
+
+    for lines in csv:
+        dict[lines['Job Class']] = float(lines['Percentage'])
+
+x = 0.0
+for i, j in dict.items():
+    if x <= randomNum < x + j:
+        print (i)
+    x += j
+     
