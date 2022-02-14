3480ed26a8880d4915717fc672e855541c3320df
Mark Zhu <markzhu926@gmail.com>
2022-02-14 08:40:36 -0500

    added files for k30

---
+++ k30_canvas/clickdraw.js
@@ -0,0 +1,33 @@
+// retrieve node in DOM via ID
+var c =
+
+// instantiate a CanvasRenderingContext2D object
+var ctx = c.getContext("2d");
+
+// init global state var
+var mode = "rect";
+
+// var toggleMode = function(e) {
+var togglemode = (e) => {
+  console.log("toggling...");
+  if () {
+
+  }
+  else {
+
+  }
+}
+
+var drawRect = function(e) {
+  var mouseX =
+  var mouseY =
+  console.log("mouseclick registered at ", mouseX, mouseY);
+}
+
+//var draw = function(e){
+var draw (e) => {
+  
+}
+
+// var wipeCanvas = function() {
+var wipeCanvas = () => {}
